524e19305877522205b160c57dd4a9d9e92da736
Kovacs Roland <kovacsroland1999@gmail.com>
2023-06-12 21:36:50 +0300

    Init vue3 typescript project and add dependencies

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+import HeroSection from './components/HeroSection.vue'
+</script>
+
+<template>
+  <div>
+    <TheHeader />
+    <TheFooter />
+    <HeroSection />
+    <HowItWorks />
+    <Card />
+    <OurTeam />
+    <TheFooter />
+  </div>
+</template>
+
+<style scoped></style>
